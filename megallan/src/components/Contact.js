5456ab7fb70fa4bfb0811cb93938f14117d5d9bf
annanoukou <anna.noukou@gmail.com>
2021-11-15 13:45:02 +0200

    Forms

--- megallan/src/components/Contact.js
+++ megallan/src/components/Contact.js
@@ -49,7 +49,7 @@ class Contact extends React.Component {
       // const sgMail = require('@sendgrid/mail');
       // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
       // const msg = {
-      //   to: 'anna.noukou@gmail.com',
+      //   to: '',
       //   from: data.email,
       //   subject: 'Sending with SendGrid is Fun',
       //   text: 'and easy to do anywhere, even with Node.js',
